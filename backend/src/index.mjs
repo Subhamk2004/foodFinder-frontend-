dfc3ee9a793b18919924a3349d3143cfe3128100
Subhamk2004 <subhamk1978@gmail.com>
2024-08-04 04:42:50 +0530

    changes

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -16,9 +16,15 @@ const app = express();
 const allowedOrigins = ['https://food-finder-lime.vercel.app' ,'http://localhost:5173', 'http://localhost:5000'];
 
 app.use(cors({
-  origin:'*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
 }));
 
